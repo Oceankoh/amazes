bf5cdf6e45e378381fc56f4e134786c09317d05d
River <riverkex@gmail.com>
2019-05-24 00:35:23 +0800

    Initial setup of repos

---
+++ android/app/src/main/java/ocean/a_maze_ment/MainActivity.java
@@ -0,0 +1,13 @@
+package ocean.a_maze_ment;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
