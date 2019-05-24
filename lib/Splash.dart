3969bb095a5cca61bd3f81deaac45d6b775db5f5
River <riverkex@gmail.com>
2019-05-25 00:48:34 +0800

    Basic Framework of App part 1

---
+++ lib/Splash.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:a_maze_ment/Home.dart';
+
+import 'dart:async';
+
+void main() => runApp(BaseApp());
+
+class BaseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Splash());
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  State createState() => new SplashState();
+}
+
+class SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, new MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
