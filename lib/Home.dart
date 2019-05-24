69f913ac792b6a6df8e163a99c42d8cba838e88f
River <riverkex@gmail.com>
2019-05-25 00:48:34 +0800

    Basic Framework of App part 1

---
+++ lib/Home.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
