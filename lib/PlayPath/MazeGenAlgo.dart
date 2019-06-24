c1fc2e37e9b286d4decb1817cc140532059682bf
River <riverkex@gmail.com>
2019-06-24 22:37:08 +0800

    Wrote an untested dfs algorithm to generate maze in an 2d array. Maze format is as follows. 2d array of 'blocks' blocks have updownleftright walls and visited property. added coords object for easier code as well.

--- lib/PlayPath/MazeGenAlgo.dart
+++ lib/PlayPath/MazeGenAlgo.dart
@@ -1,9 +1,78 @@
 import 'dart:math';
+import 'dart:collection';
+import 'package:a_maze_ment/Globals/DataTypes.dart';
 
-class MazeGen{
-	List<List<int>> generate(int size){
-		List<List<int>> maze=List<List<int>>(size);
+class MazeGen {
+  //variables
+  List<List<Block>> maze;
+  Queue<Coords> visited;
 
-		return null;
-	}
+  List<List<Block>> generate(int size) {
+    var rndx = Random();
+    int x = rndx.nextInt(size);
+    int y = rndx.nextInt(size);
+    maze[x][y].visited = true;
+    visited.add(Coords(x, y));
+    algo(Coords(x, y));
+    return null;
+  }
+
+  void algo(Coords coordinates) {
+    int x = coordinates.x;
+    int y = coordinates.y;
+    bool unvisited = false;
+    List<int> randomizer;
+    if (!(maze[x][y + 1].visited)) {
+      //up node
+      unvisited = true;
+      randomizer.add(1);
+    }
+    if (maze[x + 1][y].visited) {
+      //right node
+      unvisited = true;
+      randomizer.add(2);
+    }
+    if (maze[x][y - 1].visited) {
+      //down node
+      unvisited = true;
+      randomizer.add(3);
+    }
+    if (maze[x - 1][y].visited) {
+      //left node
+      unvisited = true;
+      randomizer.add(4);
+    }
+    if (unvisited) {
+      randomizer.shuffle(Random()); //possible error here that doesn't ensure randomness
+      switch (randomizer[0]) {
+        case 1: //up
+          maze[x][y].up = false;
+          maze[x][y + 1].down = false;
+          maze[x][y].visited = true;
+          algo(Coords(x, y + 1));
+          break;
+        case 2: //right
+          maze[x][y].right = false;
+          maze[x + 1][y].left = false;
+          maze[x][y].visited = true;
+          algo(Coords(x + 1, y));
+          break;
+        case 3: //down
+          maze[x][y].down = false;
+          maze[x][y - 1].up = false;
+          maze[x][y].visited = true;
+          algo(Coords(x, y - 1));
+          break;
+        case 4: //left
+          maze[x][y].left = false;
+          maze[x - 1][y].right = false;
+          maze[x][y].visited = true;
+          algo(Coords(x - 1, y));
+          break;
+      }
+    } else if (visited.isNotEmpty) {
+      Coords prev = visited.removeLast();
+      algo(prev);
+    }
+  }
 }
