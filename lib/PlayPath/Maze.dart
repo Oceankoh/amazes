7881589c8b7138b41ebc4a59da0311f59513d851
River <riverkex@gmail.com>
2019-06-24 18:55:02 +0800

    Framework of what seems to be a maze generation algorithm written in dart. Why would you do this to yourself...

--- lib/PlayPath/Maze.dart
+++ lib/PlayPath/Maze.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
 
 class GenerateMaze extends StatefulWidget {
-	final int side;
-	GenerateMaze({Key key, @required this.side}) : super(key: key);
-	@override
-	State createState() => _MazeState(side);
+  final int side;
+  GenerateMaze({Key key, @required this.side}) : super(key: key);
+  @override
+  State createState() => _MazeState(side);
 }
 
 class _MazeState extends State<GenerateMaze> {
-	int side;
-	_MazeState(int s){
-		side=s;
-	}
+  int side;
+  List<List<int>> maze;
+  _MazeState(int s) {
+    side = s;
+  }
 
-	@override
-	Widget build(BuildContext context) {
-		// TODO: implement build
-		return null;
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).canvasColor,
+        body: Container(
+          padding: EdgeInsets.all(5),
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: side),
+            itemBuilder: drawMaze,
+            itemCount: side * side,
+          ),
+        ));
+  }
+
+  Widget drawMaze(BuildContext context, int index) {
+    int x, y = 0;
+    x = (index / side).floor();
+    y = (index % side);
+    return GridTile(
+      child: drawGridCell(x, y),
+    );
+  }
+
+  Widget drawGridCell(int x, y) {
+    if (maze[x][y] == 0) {
+      //wall
+      return Container(color: Theme.of(context).accentColor);
+    } else {
+      //path
+      return Container(color: Theme.of(context).primaryColor);
+    }
+  }
 }
