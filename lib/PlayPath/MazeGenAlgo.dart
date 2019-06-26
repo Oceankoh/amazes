f9df10ddfb024f22572890500c49a1a1eea806b8
River <riverkex@gmail.com>
2019-06-26 12:33:29 +0800

    fixed dfs with nat's help maze displays properly

--- lib/PlayPath/MazeGenAlgo.dart
+++ lib/PlayPath/MazeGenAlgo.dart
@@ -9,10 +9,14 @@ class MazeGen {
   int size;
 
   List<List<Block>> generate(int s) {
-    List<Block> init = [];
     this.size = s;
-    for (int i = 0; i < size; i++) init.add(Block());
-    for (int i = 0; i < size; i++) maze.add(init);
+    for (int i = 0; i < size; i++) {
+      List<Block> init = [];
+      for (int j = 0; j < size; j++) {
+        init.add(Block());
+      }
+      maze.add(init);
+    }
     var rnd = Random();
     int xStart = rnd.nextInt(size);
     int yStart = rnd.nextInt(size);
@@ -23,64 +27,65 @@ class MazeGen {
   void algo(Coords coordinates) {
     int x = coordinates.getX();
     int y = coordinates.getY();
+
+    //mark current as visited
+    maze[x][y].visited = true;
+
     bool unvisited = false;
     List<int> randomizer = [];
+
+    //check for adjacent visited nodes
     if (y + 1 < size) {
-      if (!(maze[x][y + 1].getvisited())) {
+      if (!(maze[x][y + 1].getVisited())) {
         //up node
         unvisited = true;
         randomizer.add(1);
       }
     }
     if (x + 1 < size) {
-      if (!(maze[x + 1][y].getvisited())) {
+      if (!(maze[x + 1][y].getVisited())) {
         //right node
         unvisited = true;
         randomizer.add(2);
       }
     }
     if (y - 1 >= 0) {
-      if (!(maze[x][y - 1].getvisited())) {
+      if (!(maze[x][y - 1].getVisited())) {
         //down node
         unvisited = true;
         randomizer.add(3);
       }
     }
     if (x - 1 >= 0) {
-      if (!(maze[x - 1][y].getvisited())) {
+      if (!(maze[x - 1][y].getVisited())) {
         //left node
         unvisited = true;
         randomizer.add(4);
       }
     }
-    if (unvisited) {
-      randomizer.shuffle(Random()); //possible error here that doesn't ensure randomness
+    if (unvisited && randomizer.length>0) {
+      randomizer.shuffle(Random());
       switch (randomizer[0]) {
         case 1: //up
           maze[x][y].up = false;
           maze[x][y + 1].down = false;
-          maze[x][y].visited = true;
           visited.add(Coords(x, y));
           algo(Coords(x, y + 1));
           break;
         case 2: //right
           maze[x][y].right = false;
           maze[x + 1][y].left = false;
-          maze[x][y].visited = true;
           visited.add(Coords(x, y));
           algo(Coords(x + 1, y));
           break;
         case 3: //down
           maze[x][y].down = false;
           maze[x][y - 1].up = false;
-          maze[x][y].visited = true;
-          visited.add(Coords(x, y));
           algo(Coords(x, y - 1));
           break;
         case 4: //left
           maze[x][y].left = false;
           maze[x - 1][y].right = false;
-          maze[x][y].visited = true;
           visited.add(Coords(x, y));
           algo(Coords(x - 1, y));
           break;
