b6861704119e1ee40dee26ccb78da80ca53cdc43
Oceankoh <riverkex@gmail.com>
2019-07-19 03:09:13 +0800

    Major change to maze specs. Got rid of long list of buttons in exchange for slider design. Much cleaner. Some themeing stuff also.

--- lib/PreApp/Start.dart
+++ lib/PreApp/Start.dart
@@ -14,9 +14,11 @@ class BaseApp extends StatelessWidget {
             buttonColor: Color(0x00FFFFFF),
             accentColor: Color.fromRGBO(51, 51, 204, 1),
             canvasColor: Color(0xFF263238),
+            accentTextTheme: TextTheme(body2: TextStyle(color: Colors.black,fontWeight: FontWeight.bold)),
             textTheme: TextTheme(
                 headline: TextStyle(fontSize: 50, color: Colors.white,fontWeight: FontWeight.bold),
                 title: TextStyle(fontSize: 30, color: Colors.black),
+                caption: TextStyle(fontSize: 20, color: Colors.white),
                 body1: TextStyle(fontSize: 10, color: Colors.white),
                 body2: TextStyle(fontSize: 5, color: Colors.white),
                 button: TextStyle(fontSize: 20, color: Colors.white)),
