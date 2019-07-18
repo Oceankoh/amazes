8a5751cbac34978993198207e22dc422515e4a77
Oceankoh <riverkex@gmail.com>
2019-07-19 01:45:27 +0800

    Minor Themeing to app Added animations to home screen.

--- lib/PreApp/Start.dart
+++ lib/PreApp/Start.dart
@@ -9,17 +9,18 @@ class BaseApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-            brightness: Brightness.light,
+            brightness: Brightness.dark,
             primaryColor: Colors.white,
+            buttonColor: Color(0x00FFFFFF),
             accentColor: Color.fromRGBO(51, 51, 204, 1),
-            canvasColor: Color(0xff576574),
+            canvasColor: Color(0xFF263238),
             textTheme: TextTheme(
                 headline: TextStyle(fontSize: 50, color: Colors.white,fontWeight: FontWeight.bold),
                 title: TextStyle(fontSize: 30, color: Colors.black),
                 body1: TextStyle(fontSize: 10, color: Colors.white),
                 body2: TextStyle(fontSize: 5, color: Colors.white),
-                button: TextStyle(fontSize: 20, color: Colors.black)),
-            buttonTheme: ButtonThemeData(height: 50, minWidth: 200, padding: EdgeInsets.all(10))),
+                button: TextStyle(fontSize: 20, color: Colors.white)),
+            buttonTheme: ButtonThemeData(height: 50, minWidth: 200, padding: EdgeInsets.all(10),buttonColor: Colors.transparent)),
         home: Splash());
   }
 }
