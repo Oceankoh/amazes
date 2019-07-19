d670a1a49136c06ffc5d59fcbe289eb9d127a24e
Oceankoh <riverkex@gmail.com>
2019-07-19 23:17:58 +0800

    Added shadows and kewl stuff to buttons. Themed more stuff for about page.

--- lib/AboutPath/About.dart
+++ lib/AboutPath/About.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:a_maze_ment/PreApp/Home.dart';
+import 'package:a_maze_ment/Globals/device.dart' as dev;
 
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('Title',style: Theme.of(context).textTheme.title),
-          Text('Body',style: Theme.of(context).textTheme.body1,),
-          MaterialButton(onPressed: () {
-            Navigator.push(context, new MaterialPageRoute(builder: (context) => HomePage()));
-          })
+          Padding(
+              padding: EdgeInsets.only(left: dev.screenWidth * 0.1, top: dev.screenHeight * 0.15),
+              child: Text('About', style: Theme.of(context).textTheme.title)),
+          Padding(
+              padding: EdgeInsets.all(dev.screenWidth * 0.1),
+              child: Text(
+                'Body',
+                style: Theme.of(context).textTheme.body1,
+              )),
+          Padding(
+              child: MaterialButton(
+                  height: 50,
+                  minWidth: 200,
+                  onPressed: () {
+                    Navigator.push(
+                        context, new MaterialPageRoute(builder: (context) => HomePage()));
+                  },
+                  padding: EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                  child: Text('Return To Home', style: Theme.of(context).textTheme.button)),
+              padding: EdgeInsets.all(20))
         ],
       ),
     );
--- lib/PreApp/Start.dart
+++ lib/PreApp/Start.dart
@@ -12,16 +12,17 @@ class BaseApp extends StatelessWidget {
             brightness: Brightness.dark,
             primaryColor: Colors.white,
             buttonColor: Color(0x00FFFFFF),
-            accentColor: Color.fromRGBO(51, 51, 204, 1),
+            primaryColorDark: Color(0xFF000A12),
+            accentColor: Color(0xFF003C8F),
             canvasColor: Color(0xFF263238),
             accentTextTheme: TextTheme(body2: TextStyle(color: Colors.black,fontWeight: FontWeight.bold)),
             textTheme: TextTheme(
                 headline: TextStyle(fontSize: 50, color: Colors.white,fontWeight: FontWeight.bold),
-                title: TextStyle(fontSize: 30, color: Colors.white),
+                title: TextStyle(fontSize: 40, color: Colors.white),
                 caption: TextStyle(fontSize: 20, color: Colors.white),
-                body1: TextStyle(fontSize: 10, color: Colors.white),
+                body1: TextStyle(fontSize: 20, color: Colors.white),
                 body2: TextStyle(fontSize: 5, color: Colors.white),
-                button: TextStyle(fontSize: 20, color: Colors.white)),
+                button: TextStyle(fontSize: 20, color: Colors.white,shadows: [Shadow(color: Color(0xFF000A12),offset: Offset(5,5),blurRadius: 10)])),
             buttonTheme: ButtonThemeData(height: 50, minWidth: 200, padding: EdgeInsets.all(10),buttonColor: Colors.transparent)),
         home: Splash());
   }
