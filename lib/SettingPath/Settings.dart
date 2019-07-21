dde0b34e4dc46f34314017d948c88c628cd2c427
Oceankoh <riverkex@gmail.com>
2019-07-21 22:25:39 +0800

    Started on UI for settings page. Linked Home to Settings. Need to implement Audio feature soon. minor ui changes to about page.

--- lib/SettingPath/Settings.dart
+++ lib/SettingPath/Settings.dart
@@ -1,42 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:a_maze_ment/Globals/device.dart' as dev;
 
-class SettingsPage extends StatelessWidget {
+class Settings extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => SettingsPage();
+}
+
+class SettingsPage extends State<Settings> {
+  int bgAudio = 100, gameAudio = 100;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).canvasColor,
-      body: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text(
-                "Background Audio",
+        backgroundColor: Theme.of(context).canvasColor,
+        body: Column(children: [
+          Container(
+              child: Text(
+            "Background Audio:",
+          )),
+          Container(
+            child: Slider(
+              value: bgAudio.toDouble(),
+              label: '$bgAudio',
+              onChanged: (double value) {
+                setState(() {
+                  bgAudio = value.round();
+                });
+              },
+              min: 0,
+              max: 100,
+              divisions: 100,
+            ),
+            padding: EdgeInsets.all(dev.screenWidth * 0.05),
+          ),
+          Container(
+              child: Text(
+                "Game Audio:",
               ),
-              Slider(value: null, onChanged: null)
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text(
-                "Game Audio",
-              ),
-              Slider(value: null, onChanged: null)
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text("Button Controls"),
-              Switch(value: null, onChanged: null)
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Text("Swipe Controls"),
-              Switch(value: null, onChanged: null)
-            ],
-          )
-        ],
-      ),
-    );
+              padding: EdgeInsets.only(top: dev.screenHeight * 0.1)),
+          Container(
+              child: Slider(
+                  value: gameAudio.toDouble(),
+                  label: '$gameAudio',
+                  onChanged: (double value) {
+                    setState(() {
+                      gameAudio = value.round();
+                    });
+                  },
+                  min: 0,
+                  max: 100),
+              padding: EdgeInsets.all(dev.screenWidth * 0.05))
+        ], mainAxisAlignment: MainAxisAlignment.center));
   }
 }
