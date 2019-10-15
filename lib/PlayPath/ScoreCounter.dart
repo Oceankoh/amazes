a33b50fc727a764ae7ae356f1a17ec1135015e83
Ocean <riverkex@gmail.com>
2019-10-15 23:57:08 +0800

    Modified scoring formula

--- lib/PlayPath/ScoreCounter.dart
+++ lib/PlayPath/ScoreCounter.dart
@@ -14,12 +14,10 @@ class ScoreObject{
 	}
 	calculate(int mazeSize){
 		double timeTaken= _time.elapsedMilliseconds.toDouble()/1000;
-		double curve = 10*mazeSize*mazeSize*exp(-(timeTaken).toDouble()/(mazeSize*mazeSize));
-		_score = curve.round();
+		double curve = pow(mazeSize,e)*exp(-(timeTaken).toDouble()/pow(mazeSize,e));
+		_score = curve.truncate();
 		_time.reset();
-		print(timeTaken);
-		print(curve.round());
-		print(mazeSize);
+		print(curve);
 	}
 
 	int get score => _score;
