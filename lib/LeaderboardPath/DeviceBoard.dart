d629b73273c14daf000c62f1fcfde6f51b64cac3
Ocean <riverkex@gmail.com>
2019-10-16 08:33:10 +0800

    Started on Leaderboards. Only basic navigation done

---
+++ lib/LeaderboardPath/DeviceBoard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class LocalBoard extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => LocalLeaderboard();
+}
+
+class LocalLeaderboard extends State<LocalBoard>{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
