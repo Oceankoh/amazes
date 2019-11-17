266de94c198a534ddb49970f21ac86a65610edf2
Ocean <riverkex@gmail.com>
2019-11-17 12:22:34 +0800

    Added Content to help page

--- lib/AboutPath/Help.dart
+++ lib/AboutPath/Help.dart
@@ -3,35 +3,100 @@ import 'package:aMazes/PreApp/Home.dart';
 import 'package:aMazes/Globals/device.dart' as dev;
 
 class About extends StatelessWidget {
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			body: Column(
-				mainAxisAlignment: MainAxisAlignment.start,
-				crossAxisAlignment: CrossAxisAlignment.start,
-				children: <Widget>[
-					Padding(
-						padding: EdgeInsets.only(left: dev.screenWidth * 0.1, top: dev.screenHeight * 0.15),
-						child: Text('Help', style: Theme.of(context).textTheme.title)),
-					Padding(
-						padding: EdgeInsets.all(dev.screenWidth * 0.1),
-						child: Text(
-							'Body',
-							style: Theme.of(context).textTheme.body1,
-						)),
-					Center(
-						child: MaterialButton(
-							height: 50,
-							minWidth: 200,
-							onPressed: () {
-								Navigator.pushReplacement(
-									context, new MaterialPageRoute(builder: (context) => HomePage()));
-							},
-							padding: EdgeInsets.all(dev.screenWidth*0.01),
-							shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-							child: Text('Return To Home', style: Theme.of(context).textTheme.button)))
-				],
-			),
-		);
-	}
+  @override
+  String helpGame = """
+Click on the play button.
+Select your maze size using either the slider or the manual increments beside it.
+Once selected press confirm.
+A random square maze of your specified size will be generated.
+You will start at the top left and your goal is to navigate your way down to the bottom right corner marked with the star icon.
+Use the arrow buttons below the maze to move.
+Be quick on your fingers as there is a timer running immediately after the maze is generated.
+Your score will be calculated according to your specified size and time taken to complete the maze.
+After reaching the end, you will be greeted with a winning screen to save your score.
+Enter your name into the textfield to save your score on the leaderboards.
+""";
+  String helpLeaderboard = """
+You can check the leaderboard by pressing the leaderboards button.
+You can then choose to view either the local leaderboard or an online leaderboard.
+Your score is automatically saved to the local leaderboard. You may clear the scores at any time by simply clicking on the clear leaderboard button.
+If you have previously connected to an online leaderboard, your score will also be saved there.
+To connect to a new leaderboard, you may either enter the leaderboard ID of an existing board or opt to create a new board.
+Once your board has been created you may share the boardID with your friends and compete for the top position.
+Should you choose to join another online leaderboard, you may select the leave leaderboard option.
+""";
+  String helpSettings = """
+Clicking on the settings button brings allows you to configure the game settings.
+You may increase or decrease the Game music or background music by using the sliders.
+""";
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(
+                  left: dev.screenWidth * 0.1, top: dev.screenHeight * 0.15),
+              child: Text('Help', style: Theme.of(context).textTheme.headline)),
+          Padding(
+              padding: EdgeInsets.all(dev.screenWidth * 0.1),
+              child: Text(
+                'Playing the Game',
+                style: Theme.of(context).textTheme.body1,
+              )),
+          Padding(
+              padding: EdgeInsets.fromLTRB(
+                  dev.screenWidth * 0.1, 0, dev.screenWidth * 0.1, 0),
+              child: Text(
+                helpGame,
+                style: Theme.of(context).textTheme.body1,
+              )),
+          Padding(
+              padding: EdgeInsets.all(dev.screenWidth * 0.1),
+              child: Text(
+                'Leaderboards',
+                style: Theme.of(context).textTheme.body1,
+              )),
+          Padding(
+              padding: EdgeInsets.fromLTRB(
+                  dev.screenWidth * 0.1, 0, dev.screenWidth * 0.1, 0),
+              child: Text(
+                helpLeaderboard,
+                style: Theme.of(context).textTheme.body1,
+              )),
+          Padding(
+              padding: EdgeInsets.all(dev.screenWidth * 0.1),
+              child: Text(
+                'Settings',
+                style: Theme.of(context).textTheme.body1,
+              )),
+          Padding(
+              padding: EdgeInsets.fromLTRB(
+                  dev.screenWidth * 0.1, 0, dev.screenWidth * 0.1, 0),
+              child: Text(
+                helpSettings,
+                style: Theme.of(context).textTheme.body1,
+              )),
+          Center(
+              child: MaterialButton(
+                  height: 50,
+                  minWidth: 200,
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                        context,
+                        new MaterialPageRoute(
+                            builder: (context) => HomePage()));
+                  },
+                  padding: EdgeInsets.all(dev.screenWidth * 0.01),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Text('Return To Home',
+                      style: Theme.of(context).textTheme.button)))
+        ],
+      ),
+    ));
+  }
 }
