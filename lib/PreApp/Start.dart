1f493ad6b361a4e56b14a0fb4ea00742edba9de0
Ocean <riverkex@gmail.com>
2019-11-17 23:11:50 +0800

    Added extra line to help page
    Messed with styles to beautify help page

--- lib/PreApp/Start.dart
+++ lib/PreApp/Start.dart
@@ -1,4 +1,7 @@
+import 'dart:ui' as prefix1;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
 import 'package:flutter/services.dart';
 import 'package:aMazes/PreApp/Splash.dart';
 import 'dart:ui';
@@ -25,7 +28,8 @@ class BaseApp extends StatelessWidget {
                 headline: TextStyle(fontSize: 50, color: Colors.white,fontWeight: FontWeight.bold),
                 title: TextStyle(fontSize: 25, color: Colors.black),
                 caption: TextStyle(fontSize: 20, color: Colors.white),
-                body1: TextStyle(fontSize: 20, color: Colors.white),
+                subhead: TextStyle(fontSize:20, color: Colors.white, fontWeight: FontWeight.bold),
+                body1: TextStyle(fontSize: 17, color: Colors.white),
                 body2: TextStyle(fontSize: 15, color: Colors.white),
                 button: TextStyle(fontSize: 20, color: Colors.white,shadows: [Shadow(color: Color(0xFF000A12),offset: Offset(5,5),blurRadius: 10)])),
             buttonTheme: ButtonThemeData(height: 50, minWidth: 200, padding: EdgeInsets.all(10),buttonColor: Colors.transparent)),
